ec93cf072ea5e11260f42e4c0b255329c04aeb6e
Moshe Itzhaki <moshe.z.itzhaki@gmail.com>
2016-11-30 04:54:39 +0200

    Sorcery authentication complete

--- biogger/app/controllers/articles_controller.rb
+++ biogger/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_filter :require_login, only: [:destroy, :create, :new, :edit, :update]
     include ArticlesHelper
     def index
         @articles = Article.all
